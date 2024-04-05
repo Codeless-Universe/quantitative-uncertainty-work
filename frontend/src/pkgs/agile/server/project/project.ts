ddb334d179fd3c237a45ff8375083889b508cecd
Lei Gao <gaolei@codelessuniverse.com>
2024-04-05 20:06:33 +0800

    feat: add member

--- frontend/src/pkgs/agile/server/project/project.ts
+++ frontend/src/pkgs/agile/server/project/project.ts
@@ -14,6 +14,25 @@ export const projectRouter = createTRPCRouter({
           createdAccountId: ctx.user.id,
         },
       });
+      await db.project_member.create({
+        data: {
+          accountId: ctx.user.id,
+          projectId: res.id,
+          role: "OWNER",
+        },
+      });
+      return res;
+    }),
+  myProjects: publicProcedure
+    .input(z.object({ name: z.string().min(1), introduce: z.string().optional() }))
+    .mutation(async ({ input, ctx }) => {
+      const res = await db.project.create({
+        data: {
+          name: input.name,
+          introduce: input.introduce,
+          createdAccountId: ctx.user.id,
+        },
+      });
       return res;
     }),
 });
