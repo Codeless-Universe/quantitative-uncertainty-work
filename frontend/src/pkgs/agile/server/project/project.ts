ef50c7d1b79e683ad61f2a602383d07e2e08a2c4
Lei Gao <gaolei@codelessuniverse.com>
2024-04-05 20:24:20 +0800

    feat: add update

--- frontend/src/pkgs/agile/server/project/project.ts
+++ frontend/src/pkgs/agile/server/project/project.ts
@@ -4,6 +4,17 @@ import { createTRPCRouter, publicProcedure } from "@/server/api/trpc";
 import { db } from "@/pkgs/prisma/server/db";
 import { project } from "@prisma/client";
 
+const queryIsOwner = async (projectId: string, accountId: string) => {
+  const res = await db.project_member.findFirst({
+    where: {
+      projectId,
+      accountId,
+      role: "OWNER",
+    },
+  });
+  return res !== null;
+};
+
 export const projectRouter = createTRPCRouter({
   create: publicProcedure
     .input(z.object({ name: z.string().min(1), introduce: z.string().optional() }))
@@ -24,23 +35,45 @@ export const projectRouter = createTRPCRouter({
       });
       return res;
     }),
-  myProjects: publicProcedure
-    .input(z.object({ name: z.string().min(1), introduce: z.string().optional() }))
-    .mutation(async ({ input, ctx }) => {
-      const res = await db.project_member.findMany({
-        where: {
-          accountId: ctx.user.id,
-          deleted: false,
-        },
-        include: {
-          project: true,
-        },
-      });
 
-      const retList: project[] = [];
-      res.forEach((item) => {
-        retList.push(item.project);
+  update: publicProcedure
+    .input(z.object({ id: z.string(), name: z.string().min(1), introduce: z.string().optional() }))
+    .mutation(async ({ input, ctx }) => {
+      const isOwner = await queryIsOwner(input.id, ctx.user.id);
+      if (!isOwner) {
+        return {
+          code: 2,
+          msg: "Only project owner can update.",
+        };
+      }
+      const res = await db.project.update({
+        data: {
+          name: input.name,
+          introduce: input.introduce,
+          createdAccountId: ctx.user.id,
+        },
+        where: {
+          id: input.id,
+        },
       });
-      return retList;
+      return { code: 0, msg: "success" };
     }),
+
+  myProjects: publicProcedure.input(z.object({ status: z.string().optional() })).query(async ({ input, ctx }) => {
+    const res = await db.project_member.findMany({
+      where: {
+        accountId: ctx.user.id,
+        deleted: false,
+      },
+      include: {
+        project: true,
+      },
+    });
+
+    const retList: project[] = [];
+    res.forEach((item) => {
+      retList.push(item.project);
+    });
+    return retList;
+  }),
 });
