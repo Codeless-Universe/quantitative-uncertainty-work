4afb3460b4e30418a8ea59d297867e4ebdf3e249
Lei Gao <gaolei@codelessuniverse.com>
2024-04-07 09:24:12 +0800

    feat: add tip

--- frontend/src/app/project/[projectId]/aglie-poker/edit/EditForm.tsx
+++ frontend/src/app/project/[projectId]/aglie-poker/edit/EditForm.tsx
@@ -5,39 +5,36 @@ import FormSubmitButton from "@/pkgs/base/components/form/item/FormSubmitButton"
 import FormTextarea from "@/pkgs/base/components/form/item/FormTextarea";
 import { useRouterHelper } from "@/pkgs/base/helper/useRouterHelper";
 import { api } from "@/pkgs/trpc/react";
+import { useParams } from "next/navigation";
 import { useContext } from "react";
 
 export default function EditForm({ ...props }: { onSubmitDone?: () => void }) {
-  const create = api.project.create.useMutation();
-  const update = api.project.update.useMutation();
+  const create = api.agilePoker.create.useMutation();
 
-  const { param, router } = useRouterHelper({
-    id: "",
-  });
+  const params = useParams<{ projectId: string }>();
+  const routerHelper = useRouterHelper({});
   const formContext: TFormContext = useContext(FormContext);
 
   let formWrapHander = FormWrapHander(async () => {
     return {
       name: "",
-      introduce: "",
+      meetingLink: "",
+      task: "",
     };
   });
 
   formWrapHander.onSubmit = async (formData) => {
-    const data = { ...formData, ...param };
-    if (data.id) {
-      await update.mutateAsync(data);
-    } else {
-      await create.mutateAsync(data);
-    }
+    const data = { ...formData, projectId: params.projectId };
+    const res = await create.mutateAsync(data);
     props.onSubmitDone && props.onSubmitDone();
-    router.push("/my/project");
+    routerHelper.router.push("/project" + params.projectId + "/aglie-poker/detail/" + res.id);
   };
 
   return (
     <FormWrap formWrapHanderInstance={formWrapHander} className="flex flex-col gap-3">
       <FormInput field="name" label="Name" rules={{ ...FormRole.minLength(2) }} variant="bordered" />
-      <FormTextarea field="introduce" label="Introduce" variant="bordered" rules={{}} />
+      <FormInput field="meetingLink" label="MeetingLink" rules={{ ...FormRole.minLength(2) }} variant="bordered" />
+      <FormTextarea field="task" label="Task" variant="bordered" rules={{}} />
       <div className="flex flex-row justify-start gap-2 py-4">
         <FormSubmitButton color="primary" type="submit">
           OK
